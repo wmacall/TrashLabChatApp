0424ff246eaa4d8d6df9c353f1b109a1dd84def6
wmacall <macallwalter@gmail.com>
2024-09-20 12:42:18 -0600

    New design for signup

--- app/signUp.tsx
+++ app/signUp.tsx
@@ -1,177 +1,123 @@
+import {Controller, SubmitHandler, useForm} from 'react-hook-form';
+import {useRouter} from 'expo-router';
+import {useAuthContext} from '@/context/auth.context';
 import {
-  ActivityIndicator,
+  Button,
+  ButtonText,
+  Heading,
+  Input,
   KeyboardAvoidingView,
   Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  TextInputProps,
   View,
-} from 'react-native';
-import {Control, SubmitHandler, useController, useForm} from 'react-hook-form';
-import {useRouter} from 'expo-router';
-import {Entypo} from '@expo/vector-icons';
-import {useAuthContext} from '@/context/auth.context';
+} from '@gluestack-ui/themed';
+import {InputField} from '@gluestack-ui/themed';
+import {Image} from '@gluestack-ui/themed';
+import {ButtonSpinner} from '@gluestack-ui/themed';
+import {Ionicons} from '@expo/vector-icons';
 
-interface SignInValues {
+interface SignUpValues {
   email: string;
   password: string;
   username: string;
 }
 
-interface InputProps extends TextInputProps {
-  control: Control<SignInValues, any>;
-  name: keyof SignInValues;
-}
-
-const Input = ({control, name, ...rest}: InputProps) => {
-  const {field} = useController({
-    control,
-    name,
-  });
-  return (
-    <TextInput
-      placeholder="Email"
-      value={field.value}
-      onChangeText={field.onChange}
-      style={inputStyles.input}
-      placeholderTextColor="#9e9e9e"
-      {...rest}
-    />
-  );
-};
-
-const inputStyles = StyleSheet.create({
-  input: {
-    padding: 16,
-    borderRadius: 16,
-    marginVertical: 8,
-    backgroundColor: '#eeeeee',
-  },
-});
-
 export default function SignUp() {
   const {back} = useRouter();
-  const {isLoading, onRegister} = useAuthContext();
-  const {control, handleSubmit} = useForm<SignInValues>({
+  const {onRegister, isLoading} = useAuthContext();
+  const {control, handleSubmit} = useForm<SignUpValues>({
     defaultValues: {
       email: '',
       password: '',
-      username: '',
     },
   });
 
-  const handleSignUp: SubmitHandler<SignInValues> = values =>
+  const handleSignUp: SubmitHandler<SignUpValues> = values =>
     onRegister(values.email, values.username, values.password);
 
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <Pressable style={styles.containerBackButton} onPress={back}>
-        <Entypo name="chevron-thin-left" size={24} color="#424242" />
-      </Pressable>
-      <View style={styles.containerSignIn}>
-        <Text style={styles.textLogin}>Sign Up</Text>
-        <Input
-          control={control}
-          name="email"
-          keyboardType="email-address"
-          placeholder="Email"
-          autoCapitalize="none"
-        />
-        <Input
-          control={control}
-          name="username"
-          placeholder="Username"
-          autoCapitalize="none"
-        />
-        <Input
-          control={control}
-          name="password"
-          secureTextEntry
-          placeholder="Password"
-        />
-        <Pressable
-          style={styles.buttonSignIn}
-          onPress={handleSubmit(handleSignUp)}>
-          {isLoading ? (
-            <ActivityIndicator color="#fff" />
-          ) : (
-            <Text style={styles.textButtonSignIn}>Sign Up</Text>
-          )}
+    <KeyboardAvoidingView behavior="padding" flex={1}>
+      <View
+        py="$4"
+        bg="$primary700"
+        alignItems="center"
+        justifyContent="center">
+        <Pressable onPress={back} position="absolute" left={0} p="$4">
+          <Ionicons name="arrow-back-sharp" size={24} color="white" />
         </Pressable>
+        <Heading
+          textAlign="center"
+          fontSize={24}
+          fontWeight="bold"
+          zIndex={10}
+          color="white">
+          Sign Up
+        </Heading>
+      </View>
+      <View
+        px="$4"
+        flex={1}
+        flexDirection="column"
+        justifyContent="space-between"
+        pb="$4">
+        <View flex={1} alignItems="center" justifyContent="center">
+          <Image
+            source={require('../assets/images/logo.png')}
+            resizeMode="contain"
+            alt="Logo"
+            h={120}
+            w={200}
+          />
+          <Controller
+            render={({field}) => (
+              <Input mt="$4" rounded="$2xl" size="md">
+                <InputField
+                  placeholder="Enter your email"
+                  value={field.value}
+                  onChangeText={field.onChange}
+                  keyboardType="email-address"
+                />
+              </Input>
+            )}
+            name="email"
+            control={control}
+          />
+          <Controller
+            render={({field}) => (
+              <Input mt="$4" rounded="$2xl" size="md">
+                <InputField
+                  placeholder="Enter your username"
+                  value={field.value}
+                  onChangeText={field.onChange}
+                />
+              </Input>
+            )}
+            name="username"
+            control={control}
+          />
+          <Controller
+            render={({field}) => (
+              <Input mt="$4" rounded="$2xl" size="md">
+                <InputField
+                  type="password"
+                  placeholder="Enter your password"
+                  value={field.value}
+                  onChangeText={field.onChange}
+                />
+              </Input>
+            )}
+            name="password"
+            control={control}
+          />
+        </View>
+        <View>
+          <Button
+            bg="$primary700"
+            onPress={handleSubmit(handleSignUp)}
+            rounded="$xl">
+            {!isLoading ? <ButtonText>Register</ButtonText> : <ButtonSpinner />}
+          </Button>
+        </View>
       </View>
     </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 24,
-  },
-  containerImage: {
-    flex: 1,
-  },
-  containerBackButton: {
-    backgroundColor: '#e0e0e0',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 48,
-    height: 48,
-    borderRadius: 24,
-  },
-  image: {
-    width: 300,
-    height: 300,
-  },
-  containerSignIn: {
-    flex: 1,
-    paddingTop: 64,
-    paddingBottom: 64,
-  },
-  textLogin: {
-    fontSize: 32,
-    fontWeight: '500',
-    marginBottom: 72,
-  },
-  buttonSignIn: {
-    backgroundColor: '#3f51b5',
-    height: 48,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-    marginTop: 48,
-  },
-  textButtonSignIn: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  containerOr: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginVertical: 16,
-  },
-  line: {
-    height: 1,
-    flex: 1,
-    backgroundColor: '#9e9e9e',
-  },
-  orText: {
-    marginHorizontal: 16,
-    color: '#757575',
-  },
-  buttonSignUp: {
-    backgroundColor: '#e0e0e0',
-    height: 48,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-  },
-  textButtonSignUp: {
-    color: '#616161',
-    fontSize: 16,
-    fontWeight: '500',
-  },
-});
